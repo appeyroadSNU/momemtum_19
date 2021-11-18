b96a15d9720a8601100d98b5af1285ccb8de181e
MinJungi <appeyroadSNU@gmail.com>
2021-11-19 00:20:05 +0900

    211112

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -12,10 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           textTheme: Theme.of(context).textTheme.apply(
-            bodyColor: Colors.brown,
-            displayColor: Colors.white,
-          )
-      ),
+                bodyColor: Colors.brown,
+                displayColor: Colors.white,
+              )),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
@@ -34,41 +33,41 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackGround(
-        imagePath: 'images/background.jpg' ,
+        imagePath: 'images/background.jpg',
         child: Padding(
           padding: EdgeInsets.all(20.0),
-          child: Stack(
-              children:[
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Links(),
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Weather(),
-                ),
-                Align(
-                  alignment: Alignment(0.0, 0.5),
-                  child: Focus(),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Setting(),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Quotes(),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Todo(),
-                ),
-                Align(
-                    alignment: Alignment.center,
-                    child: Text("13:00", style: TextStyle(fontSize: 50, color: Colors.black),)
-                ),
-              ]
-          ),
+          child: Stack(children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Links(),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Weather(),
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.5),
+              child: Focus(),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Setting(),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Quotes(),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Todo(),
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "13:00",
+                  style: TextStyle(fontSize: 50, color: Colors.black),
+                )),
+          ]),
         ),
       ),
     );
@@ -101,7 +100,10 @@ class Weather extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Icon(Icons.cloud, color: Colors.white),
-            Container(width: 10, height:0,),
+            Container(
+              width: 10,
+              height: 0,
+            ),
             Text("21º"),
           ],
         ),
@@ -117,12 +119,14 @@ class Focus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Focus", style: TextStyle(color: Colors.black),);
+    return Text(
+      "Focus",
+      style: TextStyle(color: Colors.black),
+    );
   }
 }
 
 class Setting extends StatefulWidget {
-
   const Setting({Key? key}) : super(key: key);
 
   @override
@@ -145,14 +149,16 @@ class _SettingState extends State<Setting> {
             height: 500,
             color: Colors.green,
             child: Center(
-              child: Icon(Icons.access_time_outlined, size:50 ),
+              child: Icon(Icons.access_time_outlined, size: 50),
             ),
           ),
         ),
-
         IconButton(
-          icon: Icon(Icons.settings, color: Colors.brown,),
-          onPressed: (){
+          icon: Icon(
+            Icons.settings,
+            color: Colors.brown,
+          ),
+          onPressed: () {
             setState(() {
               _settingVisible = !_settingVisible;
             });
@@ -162,12 +168,16 @@ class _SettingState extends State<Setting> {
     );
   }
 }
+
 class Quotes extends StatelessWidget {
   const Quotes({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("Quotes", style: TextStyle(color: Colors.black),);
+    return Text(
+      "Quotes",
+      style: TextStyle(color: Colors.black),
+    );
   }
 }
 
@@ -192,7 +202,7 @@ class _TodoState extends State<Todo> {
         //   duration: Duration(milliseconds: 200),
         // ),
         AnimatedOpacity(
-          opacity: _todoVisible?1:0,
+          opacity: _todoVisible ? 1 : 0,
           duration: Duration(milliseconds: 200),
           child: Visibility(
             visible: _todoVisible,
@@ -201,19 +211,33 @@ class _TodoState extends State<Todo> {
         ),
         FlatButton(
             minWidth: 0,
-            onPressed: (){
+            onPressed: () {
               setState(() {
                 _todoVisible = !_todoVisible;
               });
             },
-            child: Text("Todo", style: TextStyle(color: Colors.black),)),
+            child: Text(
+              "Todo",
+              style: TextStyle(color: Colors.black),
+            )),
       ],
     );
   }
 }
 
-class TodoBox extends StatelessWidget {
+class TodoBox extends StatefulWidget {
   const TodoBox({Key? key}) : super(key: key);
+
+  @override
+  _TodoBoxState createState() => _TodoBoxState();
+}
+
+class _TodoBoxState extends State<TodoBox> {
+  List<String> dropDownList = ['Inbox', 'Today', 'Done'];
+  String? dropDownValue = "Inbox";
+  List<bool> showingScreen = [true, false, false];
+  int todoIndex = 0;
+  bool showMoreBox = false;
 
   @override
   Widget build(BuildContext context) {
@@ -224,18 +248,106 @@ class TodoBox extends StatelessWidget {
       margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
       decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: const BorderRadius.all(const Radius.circular(8))
-      ),
+          borderRadius: const BorderRadius.all(const Radius.circular(8))),
       child: Stack(
         children: [
           Align(
             alignment: Alignment.topLeft,
             child: DropdownButton<String>(
-              value: "test",
-              // onChanged: (String newValue){
-              //
-              // },
-              items: [],
+              value: dropDownValue,
+              items: dropDownList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(value: value, child: Text(value,));
+              }).toList(),
+              onChanged: (String? value){
+                setState(() {
+                  dropDownValue = value;
+                  showingScreen[todoIndex] = false;
+                  todoIndex = dropDownList.indexOf(value!);
+                  showingScreen[todoIndex] = true;
+                });
+              },
+              style: TextStyle(color: Colors.black),
+              // icon: ,
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Visibility(
+              visible: showingScreen[0],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Inbox", style: TextStyle(color: Colors.black),),
+                  Text("New todo", style: TextStyle(color: Colors.black)),
+                ],
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Visibility(
+              visible: showingScreen[1],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Today", style: TextStyle(color: Colors.black),),
+                  Text("Today's work", style: TextStyle(color: Colors.black)),
+                ],
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.center,
+            child: Visibility(
+              visible: showingScreen[2],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Done", style: TextStyle(color: Colors.black),),
+                  Text("Not yet", style: TextStyle(color: Colors.black)),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Visibility(
+              visible: showMoreBox,
+              child: InkWell(
+                onTap: (){
+                  setState(() {
+                    showMoreBox = !showMoreBox;
+                  });
+                },
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.topRight,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.more_horiz, color: Colors.black,),
+                  onPressed: (){
+                    setState(() {
+                      showMoreBox = !showMoreBox;
+                    });
+                  },
+                ),
+                Visibility(
+                  visible: showMoreBox,
+                  child: MoreBox(),
+                ),
+
+              ],
             ),
           ),
         ],
@@ -244,10 +356,52 @@ class TodoBox extends StatelessWidget {
   }
 }
 
+class MoreBox extends StatefulWidget {
+  @override
+  _MoreBoxState createState() => _MoreBoxState();
+}
+
+class _MoreBoxState extends State<MoreBox> {
+  List<String> moreContent = <String>['Autofocus', 'Add integration', 'Setting'];
+  double _settingHeight = 30, _settingWidth = 110, _paddingSize = 5;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _settingHeight * moreContent.length + _paddingSize*2,
+      width: _settingWidth + _paddingSize*2,
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.all(Radius.circular(_paddingSize)),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(_paddingSize),
+        child: ListView.builder(
+          // scrollDirection: Axis.horizontal,
+          itemCount: moreContent.length,
+          itemBuilder: (BuildContext context, int index){
+            return Container(
+              height: _settingHeight,
+              width: _settingWidth,
+              child: GestureDetector(
+                child: Center(
+                  child: Text(moreContent[index], style: TextStyle(color: Colors.black),),
+                ),
+                onTap: (){
+                  print(index);
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
 
 
 class BackGround extends StatelessWidget {
-
   final Widget? child;
   final String imagePath;
 
@@ -266,4 +420,3 @@ class BackGround extends StatelessWidget {
     );
   }
 }
-
